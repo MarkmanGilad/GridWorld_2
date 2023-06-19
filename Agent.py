d7e67697af6a0ae4b84a7169e98cb5a49800e68d
MarkmanGilad <gilad@markman.co.il>
2023-06-19 23:39:45 +0300

    Merge branch 'main' of https://github.com/MarkmanGilad/GridWorld2

---
+++ Agent.py
@@ -0,0 +1,51 @@
+import random
+from typing import Any
+import numpy as np
+from Graphics import *
+from Action import Action
+from Environement import Environement as Env
+
+
+class Random_Agent:
+    def __init__(self, env) -> None:
+        self.env : Env = env
+        self.Reward = 0
+    
+    def get_action(self, state):
+        actions = self.env.get_actions(state)
+        return random.choice(actions)
+            
+    def add_reward(self, reward):
+        self.Reward += reward
+
+    def __call__(self, state):
+        return self.get_action(state)
+    
+class AI_Agent:
+    def __init__(self, env) -> None:
+        self.env : Env = env
+        self.Reward = 0
+        self.Policy = np.full((ROWS, COLS), 3)  # Random policy always down
+        self.Value = np.zeros((ROWS, COLS))     # Random Value all zero
+        self.gamma = 0.9
+
+    def get_action(self, state):
+        return Action(self.Policy[state])
+            
+    def add_reward(self, reward):
+        self.Reward += reward
+
+    def policy_eval (self):
+        pass
+
+    def Policy_improv (self):
+        pass
+
+    def Policy_Iteration (self):
+        pass
+
+    def __call__(self, state):
+        return self.get_action(state)
+    
+
+    
